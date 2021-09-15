361aee1830e9dd1e104d677416da5b4f65b4e597
Amar Nath <nthvpsn@gmail.com>
2021-09-15 22:07:41 +0530

    Day 1

---
+++ Day1/day1.py
@@ -0,0 +1,39 @@
+print('Hello World!')
+
+# Exercise 1
+print('Day 1 - The Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+print('line1\nline2\nline3')
+
+print("Amar" + " " + "Nath")
+
+# EXERCISE 2
+print('Day 1 String Manipulation')
+print('String concatenation is done with the "+" symbol')
+print('eg print("hello" + "world")')
+print("new line can be created with backslash and n")
+
+print("Hello " + input('What your name?') + "!")
+print(len(input()))
+
+# EXERCISE 3
+name = input("What is your name ? \n")
+length = len(name)
+print(name, length)
+
+# EXERCISE 4
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a " + a)
+print("b " + b)
+
+# FINAL PROJECT
+print("Welcome to Band Name Generator")
+city = input("Name of your city\n")
+pet = input("What is your pet name\n")
+print(city + " " + pet)
